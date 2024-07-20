1dd8ba11277e7232ba61d1ef97d611c4409d26eb
parasgodhani01 <godhaniparas44@gmail.com>
2024-07-19 22:33:55 -0400

    Changes in main app

--- website/app.py
+++ website/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template_string, render_template
 import sqlite3
 from werkzeug.serving import run_simple
-import pathlib
+
 
 my_connection = sqlite3.connect(r'E:\St.Clair College\Intro to Python - 100\DAB 111-W024\DAB111_project_Group_8\Database\covid_19.db')
 cursor = my_connection.cursor()
@@ -9,36 +9,37 @@ data = cursor.execute("SELECT * FROM covid_19").fetchall()
 my_connection.close()
 # print(data)
 
-# cwd = pathlib.Path.cwd()
-# base_path = pathlib.Path(r'E:\St.Clair College\Intro to Python - 100\DAB 111-W024\DAB111_project_Group_8\Database\covid_19.db')
-# db_name = "covid_19.db"
-# db_path = base_path / db_name
-# print(db_path)
-
-
 app = Flask(__name__)
-@app.route("/")
-def index():    
-    return render_template("index.html") 
+@app.route('/')
 
-@app.route("/about")
+def index():
+    data_html = data.to_html(index=False) 
+
+    return render_template_string('''
+        <!DOCTYPE html>
+        <html>
+        <body>
+            <h1>Covid 19 Data</h1>
+            <h2>This is data Of Covid 19</h2>
+            <p>My first <b>paragraph</b> .</p>
+        {{data|safe}}
+        </body>
+        </html>
+    ''', data = data_html)
+
+@app.route('/about')
 def about():
-    return render_template("about.html")
-
-@app.route("/data")
-def data():
-    con = sqlite3.connect(db_path)
-    cursor = con.cursor()
-    covid = cursor.execute("SELECT * FROM covid_19").fetchall()
-    con.close()
-
-    columns = ['row_number', 'invoice_id', 'branch', 'customer_id', 'gender', 'age',
-       'customer_type', 'credit_score', 'product_category',
-       'number_of_products', 'tax_amount', 'price', 'total_amount', 'ratings',
-       'customer_churn']
-    
-    return render_template("about.html", columns=columns, covid=covid)
-        
+    return render_template('about.html')
 
 if __name__=="__main__":
     app.run(debug=True)
+
+
+# def index():    
+#     return render_template("index.html") 
+
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
+        
+
