423c93d1b47a078c20c454d561def58b954ad93a
parasgodhani01 <godhaniparas44@gmail.com>
2024-07-19 22:42:05 -0400

    app.py changes

--- website/app.py
+++ website/app.py
@@ -1,45 +1,35 @@
 from flask import Flask, render_template_string, render_template
-import sqlite3
 from werkzeug.serving import run_simple
+import sqlite3
 
+app = Flask(__name__,template_folder='.')
 
-my_connection = sqlite3.connect(r'E:\St.Clair College\Intro to Python - 100\DAB 111-W024\DAB111_project_Group_8\Database\covid_19.db')
-cursor = my_connection.cursor()
-data = cursor.execute("SELECT * FROM covid_19").fetchall()
-my_connection.close()
-# print(data)
-
-app = Flask(__name__)
+def get_db_connection():
+    conn = sqlite3.connect(r'E:\St.Clair College\Intro to Python - 100\DAB 111-W024\DAB111_project_Group_8\Database\covid_19.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+    
 @app.route('/')
-
 def index():
+    conn = sqlite3.connect(r'E:\St.Clair College\Intro to Python - 100\DAB 111-W024\DAB111_project_Group_8\Database\covid_19.db')
+    data = conn.execute("SELECT * FROM covid_19").fetchall()
+    conn.close()
     data_html = data.to_html(index=False) 
 
     return render_template_string('''
         <!DOCTYPE html>
         <html>
         <body>
-            <h1>Covid 19 Data</h1>
-            <h2>This is data Of Covid 19</h2>
-            <p>My first <b>paragraph</b> .</p>
+            <h1>COVID 19 Data</h1>
+            <h2>This data about COVID 19 Cases, Recoveries, and Deaths by Country</h2>
+            
         {{data|safe}}
         </body>
         </html>
-    ''', data = data_html)
+    ''', data = data)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
-
-
-# def index():    
-#     return render_template("index.html") 
-
-# @app.route("/about")
-# def about():
-#     return render_template("about.html")
-        
-
+run_simple('localhost',4000, app, use_reloader=False, use_debugger=False)
